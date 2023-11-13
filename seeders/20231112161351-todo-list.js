3822010666cee2b1a399862a8dcc073c200754b8
Nana <nanakurnianingsih82716@gmail.com>
2023-11-13 15:25:15 +0700

    seeder

---
+++ seeders/20231112161351-todo-list.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const currentDate = new Date();
+
+    await queryInterface.bulkInsert("Todos", [
+      {
+        value: "Belajar Node JS",
+        userID: "1",
+        createdAt: currentDate,
+        updatedAt: currentDate,
+      },
+      {
+        value: "Belajar Express JS",
+        userID: "2",
+        createdAt: currentDate,
+        updatedAt: currentDate,
+      },
+      {
+        value: "Belajar Sequelize",
+        userID: "1",
+        createdAt: currentDate,
+        updatedAt: currentDate,
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Todos", null, {});
+  },
+};
